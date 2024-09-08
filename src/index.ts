16785686eae9e5aae76eb513980fafa1b98d2e99
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-08 19:24:09 +0530

    v0 of the server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,45 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
+function randomId() {
+  return Math.random();
+}
+
+const subscriptions: {
+  [key: string]: {
+    ws: WebSocket;
+    rooms: string[];
+  };
+} = {};
+
+wss.on("connection", function connection(userSocket) {
+  const id = randomId();
+  subscriptions[id] = {
+    ws: userSocket,
+    rooms: [],
+  };
+
+  userSocket.on("message", function message(data) {
+    const userMessage = JSON.parse(data as unknown as string);
+    if (userMessage === "SUBSCRIBE") {
+      subscriptions[id].rooms.push(userMessage.room);
+    }
+
+    if (userMessage.type === "UNSUBSCRIBE") {
+      subscriptions[id].rooms = subscriptions[id].rooms.filter(
+        (x) => x !== userMessage.room
+      );
+    }
+
+    if (userMessage.type === "sendMessage") {
+      const message = userMessage.message;
+      const roomId = userMessage.roomId;
+      Object.keys(subscriptions).forEach((userId) => {
+        const { ws, rooms } = subscriptions[userId];
+        if (rooms.includes(roomId)) {
+          ws.send(message);
+        }
+      });
+    }
   });
-  ws.send("something");
 });
